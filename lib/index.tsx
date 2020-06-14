f8bd97a4f3f46ef724422707be7fcefb15169fc9
Eyk Rehbein <eykrehbein@gmail.com>
2020-06-14 12:45:19 +0200

    initial commit

---
+++ lib/index.tsx
@@ -0,0 +1,7 @@
+export { Anchor } from "./components/Anchor";
+export { Box } from "./components/Box";
+export { Button } from "./components/Button";
+export { Image } from "./components/Image";
+export { Input } from "./components/Input";
+export { Row } from "./components/Row";
+export { Text } from "./components/Text";
